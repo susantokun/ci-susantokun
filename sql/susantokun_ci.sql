9d02d5b4946ca00edae0055fa040187cc0bf2790
susantokun <admin@susantokun.com>
2020-03-14 22:55:40 +0700

    Version 1.2 : Update Database

--- sql/susantokun_ci.sql
+++ sql/susantokun_ci.sql
@@ -16,7 +16,7 @@ CREATE TABLE `tbl_konfigurasi` (
   PRIMARY KEY (`id_konfigurasi`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-INSERT INTO `tbl_konfigurasi` VALUES (1,'SUSANTOKUN','member.png','admin.png','admin@susantokun.com','081906515912','KOMPLEK BTN Munjul No.12A 02/06, Sukaresmi, Cianjur, Jawa Barat, Indonesia (43253)','https://facebook.com/susantokundotcom','https://instagram.com/susantokun','Susanto ganteng banget loh','Susanto ganteng banget','Susanto ganteng');
+INSERT INTO `tbl_konfigurasi` VALUES (1,'SUSANTOKUN','member.png','admin.png','admin@susantokun.com','081906515912','KOMPLEK BTN Munjul No.12A 02/06, Sukaresmi, Cianjur, Jawa Barat, Indonesia (43253)','https://facebook.com/susantokundotcom','https://instagram.com/susantokun','info-susantokun, demo-susantokun, susantokun','Situs Edukasi, Tips dan Tutorial','Susantokun adalah situs edukasi seperti pelajaran dan ilmu pengetahuan, serta membahas tentang tips, tutorial, teknologi, tugas-tugas hingga berita terkini.');
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tbl_role` (
@@ -33,20 +33,18 @@ CREATE TABLE `tbl_user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_role` int(11) NOT NULL,
   `username` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `password_reset_key` varchar(100) DEFAULT NULL,
   `first_name` varchar(50) NOT NULL,
   `last_name` varchar(50) NOT NULL,
-  `email` varchar(254) NOT NULL,
+  `email` varchar(100) NOT NULL,
   `phone` varchar(12) NOT NULL,
-  `photo` varchar(255) NOT NULL,
-  `activation_code` varchar(50) NOT NULL,
-  `forgotten_password_code` varchar(50) NOT NULL,
-  `forgotten_password_time` datetime NOT NULL,
-  `remember_code` varchar(50) NOT NULL,
-  `created_on` datetime NOT NULL,
-  `last_login` datetime NOT NULL,
-  `active` int(1) NOT NULL,
+  `photo` varchar(100) NOT NULL,
+  `activated` tinyint(1) NOT NULL DEFAULT '0',
+  `last_login` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-INSERT INTO `tbl_user` VALUES (1,1,'Administrator','$2y$05$6YXr8WY51xwBYs3jJl3LE.dBFeDAVrPNbbQjWI9EAc.UrmA.nrn5C','Administrator','Susantokun','admin@mail.com','081906515912','1526456245974.png','','','0000-00-00 00:00:00','','0000-00-00 00:00:00','2018-05-22 20:58:33',1),(2,2,'Member','$2y$05$I/LMp0RtjGQYzpt.HVVJw.Z1y68BCD871T3jvl6K8wHuQxxsK1pLy','Member','Susantokun','member@mail.com','09876545678','1583991814826.png','','','0000-00-00 00:00:00','','2018-05-16 14:31:53','2018-05-22 21:15:27',1);
+INSERT INTO `tbl_user` VALUES (1,1,'admin','$2y$05$OA.OoeNHoEkbGGKazYqPU.UOaI5jmgro8x2pRSV56ClTWlDf0EEn2','','ADMIN','SUSANTOKUN','admin@mail.com','081906515912','1526456245974.png',1,'2020-03-14 22:34:49','2020-03-14 21:58:17',NULL),(2,2,'member','$2y$05$8GdJw3BVbmhN6x2t0MNise7O0xqLMCNAN1cmP6fkhy0DZl4SxB5iO','','MEMBER','SUSANTOKUN','member@mail.com','081906515912','1583991814826.png',1,'2020-03-14 22:32:04','2020-03-14 22:00:32',NULL);
